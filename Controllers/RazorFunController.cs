ebd66487505dceb3690fbbd85329e959cb5cd9c0
Raymond Xu <raymondxu92@gmail.com>
2019-04-05 00:42:49 -0700

    first commit

---
+++ Controllers/RazorFunController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace RazorFun.Controllers
+{
+  public class RazorFunController : Controller
+  {
+    [HttpGet]       //type of request
+    [Route("")]     //associated route string (exclude the leading /)
+    public ViewResult Index()
+    {
+      System.Console.WriteLine("Working!");
+      return View("Index");
+    }
+  }
+}
